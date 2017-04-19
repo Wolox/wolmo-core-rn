d82023778d4216e192e6df603760515316d085db
Sebastian Balay <snbalay@gmail.com>
2017-04-19 13:45:55 -0300

    fix package exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import WMOToastContainer from './src/WMOToast/WMOToastContainer';
-import { configColors as configWMOToastThemes } from './src/WMOToast/WMOToast.styles';
 import { actionCreators as WMOToastActions, reducer as WMOToastReducer } from './src/WMOToast/WMOToastRedux';
 
-export { WMOToastContainer, configWMOToastThemes, WMOToastActions, WMOToastReducer };
+import * as WMOColors from './src/styles/colors';
+
+export { WMOToastContainer, WMOToastActions, WMOToastReducer, WMOColors };
